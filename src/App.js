259bdf703e211c52584e80e2891c76df8d28836f
Alefy Pimentel <alefy.p.ferreira@gmail.com>
2019-09-03 07:51:48 -0300

    Learning API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import GlobalStyle from './styles/global';
 import Routes from './router';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Routes />
-    </div>
+      <GlobalStyle />
+    </>
   );
 }
 
